d40d8589c3af5fac19afc3d2b7eb1a699d7e4bc9
nacho2002 <nacho2002>
2007-03-01 14:03:44 +0000

    GrupoHerramientas: Añadida nueva herramienta de configurarEntradaSalida (por completar).

---
+++ SimTraffic/is/SimTraffic/Herramientas/HConfigurarEntradaSalida.java
@@ -0,0 +1,25 @@
+package is.SimTraffic.Herramientas;
+
+import is.SimTraffic.IModelo;
+
+public class HConfigurarEntradaSalida implements IHerramienta {
+
+	
+	
+	//Suponemos que recibimos como parametros el nodo
+	//y los valores de entrada salida
+	Nodo nodo;
+	int[] valoresFranjas;
+	
+	public int hacer(IModelo modelo) {
+		
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int deshacer(IModelo modelo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
