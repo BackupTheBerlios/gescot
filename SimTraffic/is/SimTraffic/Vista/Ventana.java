46117188d0327c82e0196e24e46dfa9f61a78a3e
darthguado <darthguado>
2007-03-02 09:36:05 +0000

    Añadido el texto en los botones y creados los oyentes, aunque todavia no hacen nada, a excepción del comando Salir.

--- SimTraffic/is/SimTraffic/Vista/Ventana.java
+++ SimTraffic/is/SimTraffic/Vista/Ventana.java
@@ -4,7 +4,14 @@ import is.SimTraffic.IModelo;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 
@@ -20,18 +27,23 @@ import javax.swing.border.BevelBorder;
 
 public class Ventana extends JFrame 
 {
+	private JMenuItem pegarMenuItem;
+	private JMenuItem copiarMenuItem;
+	private JMenu edicionMenu;
+	private JMenuItem salirMenuItem;
+	private JPanel panel;
 	private JLabel herramientasLabel;
 	private JScrollPane scrollPane;
-	private JButton button_4;
-	private JButton button_3;
-	private JButton button_2;
-	private JButton button_1;
-	private JButton button;
+	private JButton otrosButton;
+	private JButton eliminartramoButton;
+	private JButton eliminarNodoButton;
+	private JButton añadirTramoButton;
+	private JButton añadirNodoButton;
 	private JPanel panel_herramientas;
-	private JMenuItem menuItem_2;
-	private JMenuItem menuItem_1;
-	private JMenuItem menuItem;
-	private JMenu menu;
+	private JMenuItem guardarmapaMenuItem;
+	private JMenuItem cargarMapaMenuItem;
+	private JMenuItem nuevoMapaMenuItem;
+	private JMenu archivoMenu;
 	private JMenuBar menuBar;
 	private IModelo modelo;
 	
@@ -50,50 +62,124 @@ public class Ventana extends JFrame
 		menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 
-		menu = new JMenu();
-		menu.setText("New JMenu");
-		menuBar.add(menu);
+		archivoMenu = new JMenu();
+		archivoMenu.setText("Archivo");
+		menuBar.add(archivoMenu);
 
-		menuItem = new JMenuItem();
-		menuItem.setText("New JMenuItem");
-		menu.add(menuItem);
+		nuevoMapaMenuItem = new JMenuItem();
+		nuevoMapaMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{
+			
+			}
+		});
+		nuevoMapaMenuItem.setText("Nuevo Mapa");
+		archivoMenu.add(nuevoMapaMenuItem);
 
-		menuItem_1 = new JMenuItem();
-		menuItem_1.setText("New JMenuItem");
-		menu.add(menuItem_1);
+		archivoMenu.addSeparator();
 
-		menuItem_2 = new JMenuItem();
-		menuItem_2.setText("New JMenuItem");
-		menu.add(menuItem_2);
+		cargarMapaMenuItem = new JMenuItem();
+		cargarMapaMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+			
+			}
+		});
+		cargarMapaMenuItem.setText("Cargar Mapa");
+		archivoMenu.add(cargarMapaMenuItem);
+
+		guardarmapaMenuItem = new JMenuItem();
+		guardarmapaMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+			
+			}
+		});
+		guardarmapaMenuItem.setText("GuardarMapa");
+		archivoMenu.add(guardarmapaMenuItem);
+
+		archivoMenu.addSeparator();
+
+		salirMenuItem = new JMenuItem();
+		salirMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				System.exit(0);
+			}
+		});
+		salirMenuItem.setText("Salir");
+		archivoMenu.add(salirMenuItem);
+
+		edicionMenu = new JMenu();
+		edicionMenu.setText("Edición");
+		menuBar.add(edicionMenu);
+
+		copiarMenuItem = new JMenuItem();
+		copiarMenuItem.setText("Copiar");
+		edicionMenu.add(copiarMenuItem);
+
+		pegarMenuItem = new JMenuItem();
+		pegarMenuItem.setText("Pegar");
+		edicionMenu.add(pegarMenuItem);
 
 		panel_herramientas = new JPanel();
-		panel_herramientas.setLayout(new BoxLayout(panel_herramientas, BoxLayout.PAGE_AXIS));
+		panel_herramientas.setLayout(new BorderLayout());
 		getContentPane().add(panel_herramientas, BorderLayout.WEST);
 
+		panel = new JPanel();
+		final GridLayout gridLayout = new GridLayout(0, 1);
+		gridLayout.setVgap(2);
+		panel.setLayout(gridLayout);
+		panel_herramientas.add(panel, BorderLayout.NORTH);
+
 		herramientasLabel = new JLabel();
+		panel.add(herramientasLabel);
 		herramientasLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		herramientasLabel.setText("     Herramientas");
-		panel_herramientas.add(herramientasLabel);
+		herramientasLabel.setText("Herramientas");
 
-		button = new JButton();
-		button.setText("New JButton");
-		panel_herramientas.add(button);
+		añadirNodoButton = new JButton();
+		añadirNodoButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+			
+			}
+		});
+		panel.add(añadirNodoButton);
+		añadirNodoButton.setText("Añadir Nodo");
 
-		button_1 = new JButton();
-		button_1.setText("New JButton");
-		panel_herramientas.add(button_1);
+		añadirTramoButton = new JButton();
+		añadirTramoButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+			
+			}
+		});
+		panel.add(añadirTramoButton);
+		añadirTramoButton.setText("Añadir Tramo");
 
-		button_2 = new JButton();
-		button_2.setText("New JButton");
-		panel_herramientas.add(button_2);
+		eliminarNodoButton = new JButton();
+		eliminarNodoButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+			
+			}
+		});
+		panel.add(eliminarNodoButton);
+		eliminarNodoButton.setText("Eliminar Nodo");
 
-		button_3 = new JButton();
-		button_3.setText("New JButton");
-		panel_herramientas.add(button_3);
+		eliminartramoButton = new JButton();
+		eliminartramoButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+			
+			}
+		});
+		panel.add(eliminartramoButton);
+		eliminartramoButton.setText("EliminarTramo");
 
-		button_4 = new JButton();
-		button_4.setText("New JButton");
-		panel_herramientas.add(button_4);
+		otrosButton = new JButton();
+		panel.add(otrosButton);
+		otrosButton.setText("Otros...");
 
 		scrollPane = new JScrollPane();
 		getContentPane().add(scrollPane);
