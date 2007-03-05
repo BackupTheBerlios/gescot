7b78ed4edaf9dcefb9a0ba0ed6b3db5a1b6caf05
inigo <inigo>
2007-03-05 10:30:07 +0000

    Grupo Herramientas: Iñigo

--- SimTraffic/is/SimTraffic/Herramientas/HModificarNodo.java
+++ SimTraffic/is/SimTraffic/Herramientas/HModificarNodo.java
@@ -18,6 +18,9 @@ public class HModificarNodo implements IHerramienta {
 	boolean es;
 	boolean señal;
 	
+	//Nodo auxiliar para poder deshacer los cambios
+	Nodo nodoGuardado;
+	
 	
 	public HModificarNodo(Nodo nodo,ITipoElemento  tipo,Posicion pos,boolean es,boolean señal){
 		this.nodo = nodo;
@@ -28,15 +31,44 @@ public class HModificarNodo implements IHerramienta {
 	 
 	}
 	public int hacer(IModelo modelo) {
+		
+		/**
+		 * Aqui guardamos el nodo anterior,para poder deshacer cualquier cambio
+		 */
+		nodoGuardado = nodo;
+		
+		/**
+		 * En caso de que quitemos una señal de semáforo del nodo,esta linea lo comprueba
+		 * y si es asi lo borra de la lista de semaforos que mantiene el modelo
+		 */
 		if (nodo.getSeñal().getTipo()=="semaforo")
-			//modelo.setMapa(((Mapa)modelo.getMapa().eliminar(nodo.getSeñal())));
+			modelo.getMapa().eliminar(nodo.getSeñal());
+		/**
+		 * Borramos la señal o el punto de e/s
+		 * (Ojo,las señales sólo se eliminan de esta manera,para 
+		 * modificarlas lo hacemos en la herramienta HModificaSeñal)
+		 * 
+		 */
 		if(señal==false) nodo.setSeñal(null);
 	    if(es==false) nodo.setEs(null);
-		// TODO Auto-generated method stub
+	    
+	    /**
+	     * Si cambiamos el tipo de nodo o la posicion,se lo aplicamos al nodo
+	     */
+	    if (tipoNodo!=nodo.getTipoNodo()) 
+	     nodo.setTipoNodo(tipoNodo);
+	    if (pos !=nodo.getPos())
+	     nodo.setPos(pos);
+		
+	    // TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int deshacer(IModelo modelo) {
+		
+		//Deshacemos el cambio restaurando los parametros con el anterior
+		nodo = nodoGuardado;
+		
 		// TODO Auto-generated method stub
 		return 0;
 	}
