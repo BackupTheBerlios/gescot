53df4331d91021a009724c90b3c45dd4245b5cbb
pablof <pablof>
2007-03-07 17:55:15 +0000

    - Actualizada SimTrafficTestSuite

--- SimTraffic/is/SimTraffic/jUnit/SimTrafficTestSuite.java
+++ SimTraffic/is/SimTraffic/jUnit/SimTrafficTestSuite.java
@@ -7,22 +7,28 @@ package is.SimTraffic.jUnit;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import is.SimTraffic.jUnit.Mapa.*;
+import is.SimTraffic.jUnit.Herramientas.*;
 
 /**
+ * Clase de test de todo el proyecto
  * @author Grupo ISTrafico
  *
  */
 public class SimTrafficTestSuite {
 	  
+	/**
+	 * Método que añade los suite de las clases de prueba de cada paquete
+	 * de la aplicación, y ejecuta todas las pruebas
+	 * 
+	 */
 	public static Test suite() {
 		    TestSuite suite = new TestSuite();
-		    suite.addTest(MapaTestSuite.suite());		    
+		    suite.addTestSuite(ControladorTest.class);
+		    suite.addTest(MapaTestSuite.suite());
+		    suite.addTest(HerramientasTestSuite.suite());		    
 		    return suite;
 		  }
 	
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		    junit.textui.TestRunner.run(suite());
 	}
