333389b80133e7f8cb38c87d63af59514325204a
pablof <pablof>
2007-03-07 18:00:07 +0000

    - Cambios menores en MapaTestSuite

--- SimTraffic/is/SimTraffic/jUnit/Mapa/MapaTestSuite.java
+++ SimTraffic/is/SimTraffic/jUnit/Mapa/MapaTestSuite.java
@@ -3,8 +3,16 @@ package is.SimTraffic.jUnit.Mapa;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+/**
+ * Clase de prueba que llama a todos los tests del paquete Mapa
+ * 
+ */
 public class MapaTestSuite {
 
+	/**
+	 * Método suite que añade los tests de todas las clases de prueba
+	 * 
+	 */
 	public static Test suite() {
 	    TestSuite suite = new TestSuite();
 	    suite.addTestSuite(MapaTest.class);
