fbf24fdbbbffad5931e0b3dc2e9c65725e77aa62
pablof <pablof>
2007-03-09 13:57:16 +0000

    Actualizado Seleccion

---
+++ SimTraffic/is/SimTraffic/Mapa/Seleccion.java
@@ -0,0 +1,62 @@
+package is.SimTraffic.Mapa;
+
+import java.util.ArrayList;
+
+/**
+ * Clase que permite almacenar los nodos y tramos que se encuentran
+ * seleccionados.
+ * @author Grupo IS trafico
+ *
+ */
+public class Seleccion {
+	/**
+	 * Mantiene la lista de nodos seleccionados del mapa
+	 */
+	private ArrayList<Nodo> nodosSeleccionados;
+
+	/**
+	 * Mantiene la lita de señales seleccionadas del mapa
+	 */
+	private ArrayList<Señal> señalesSeleccionadas;
+
+	/**
+	 * Mantiene la lista de tramos seleccionados del Mapa
+	 */
+	private ArrayList<Tramo> tramosSeleccionados;
+	
+	public Seleccion (){
+		nodosSeleccionados = new ArrayList();
+		señalesSeleccionadas = new ArrayList();
+		tramosSeleccionados = new ArrayList(); 
+	}
+
+	public void añadirNodo(Nodo nodo){
+		this.nodosSeleccionados.add(nodo);
+	}
+	
+	
+	public ArrayList<Nodo> getNodosSeleccionados() {
+		return nodosSeleccionados;
+	}
+
+	public void setNodosSeleccionados(ArrayList<Nodo> nodosSeleccionados) {
+		this.nodosSeleccionados = nodosSeleccionados;
+	}
+
+	public ArrayList<Señal> getSeñalesSeleccionadas() {
+		return señalesSeleccionadas;
+	}
+
+	public void setSeñalesSeleccionadas(ArrayList<Señal> señalesSeleccionadas) {
+		this.señalesSeleccionadas = señalesSeleccionadas;
+	}
+
+	public ArrayList<Tramo> getTramosSeleccionados() {
+		return tramosSeleccionados;
+	}
+
+	public void setTramosSeleccionados(ArrayList<Tramo> tramosSeleccionados) {
+		this.tramosSeleccionados = tramosSeleccionados;
+	}
+	
+}
