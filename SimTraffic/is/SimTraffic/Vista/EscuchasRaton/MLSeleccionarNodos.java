a43b2e7687c73ae40ce9fc7e23619051b4c25e45
pablof <pablof>
2007-03-09 13:27:06 +0000

    Para poder seleccionar Nodos, Tramos y Señales

---
+++ SimTraffic/is/SimTraffic/Vista/EscuchasRaton/MLSeleccionarNodos.java
@@ -0,0 +1,66 @@
+package is.SimTraffic.Vista.EscuchasRaton;
+
+import is.SimTraffic.IControlador;
+import is.SimTraffic.IModelo;
+import is.SimTraffic.Vista.PanelMapa;
+
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+
+public class MLSeleccionarNodos extends EscuchaRaton{
+
+	public MLSeleccionarNodos(IModelo modelo, IControlador controlador, PanelMapa panel) {
+		super(modelo, controlador, panel);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		panel.setPuntoInicial(arg0.getPoint());
+		panel.setModoSeleccion(true);
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		panel.setModoSeleccion(false);
+		panel.recrearMapa();
+		panel.repaint();
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		Point puntoDrag = arg0.getPoint();
+		panel.setPuntoDrag(puntoDrag);
+		panel.repaint();
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
