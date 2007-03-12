85f480688f0d222a8f02d609cc151f22efb0aa1b
smoken <smoken>
2007-03-12 20:47:49 +0000

    Añadidas funciones de notificación para trabajar junto a un oyente de teclado

--- SimTraffic/is/SimTraffic/Vista/EscuchasRaton/MLSeleccionarNodos.java
+++ SimTraffic/is/SimTraffic/Vista/EscuchasRaton/MLSeleccionarNodos.java
@@ -2,23 +2,37 @@ package is.SimTraffic.Vista.EscuchasRaton;
 
 import is.SimTraffic.IControlador;
 import is.SimTraffic.IModelo;
+import is.SimTraffic.Herramientas.HEliminarNodo;
+import is.SimTraffic.Mapa.Nodo;
 import is.SimTraffic.Vista.PanelMapa;
 
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 
-public class MLSeleccionarNodos extends EscuchaRaton{
-
+public class MLSeleccionarNodos extends EscuchaRaton {
+	
+	private boolean drag;
+	
 	public MLSeleccionarNodos(IModelo modelo, IControlador controlador, PanelMapa panel) {
 		super(modelo, controlador, panel);
+		drag = false;
+		panel.setFocusable(true);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		if (this.getModificadorDeTeclado() != 17){
+			this.modelo.getMapa().limpiaSeleccion();
+		}
+		Nodo seleccionado = buscarNodo(panel.x_RepAMapa(arg0.getX()), panel.y_RepAMapa(arg0.getY()));
+		if (seleccionado != null)
+			if (modelo.getMapa().getSeleccion().getNodosSeleccionados().contains(seleccionado)){
+				modelo.getMapa().getSeleccion().getNodosSeleccionados().remove(seleccionado);
+			} else {
+				modelo.getMapa().getSeleccion().añadirNodo(seleccionado);
+			}
 	}
 
 	@Override
@@ -44,9 +58,15 @@ public class MLSeleccionarNodos extends EscuchaRaton{
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		panel.setModoSeleccion(false);
-		//panel.recrearMapa();
 		panel.repaint();
-		panel.notificaSeleccion(0);
+		
+		if (this.getModificadorDeTeclado() != 17 && drag)
+			this.modelo.getMapa().limpiaSeleccion();
+
+		if (drag)
+			panel.notificaSeleccion(0);
+
+		drag = false;
 	}
 
 	@Override
@@ -55,6 +75,7 @@ public class MLSeleccionarNodos extends EscuchaRaton{
 		Point puntoDrag = arg0.getPoint();
 		panel.setPuntoDrag(puntoDrag);
 		panel.repaint();
+		drag = true;
 
 	}
 
@@ -63,5 +84,21 @@ public class MLSeleccionarNodos extends EscuchaRaton{
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	/* (non-Javadoc)
+	 * Sobreescribo el método del padre para añadir funciones extra.
+	 * @see is.SimTraffic.Vista.EscuchasRaton.EscuchaRaton#notificar(int)
+	 */
+	public void notificar(int modificador){
+		super.notificar(modificador);
+		System.out.print(modificador);
+		// Si se ha pulsado la tecla "Supr"
+		if (modificador == 127){
+			//Crear una herramienta de borrado de varios nodos
+			//y aplicarla al modelo.
+			
+			//(es una idea).
+			
+		}
+	}
 }
