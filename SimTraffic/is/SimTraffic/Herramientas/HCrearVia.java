d98adb69f711d53eab71ae35d34b70c0952a3aff
inigo <inigo>
2007-03-16 11:30:01 +0000

    Añadido deshacer Via

--- SimTraffic/is/SimTraffic/Herramientas/HCrearVia.java
+++ SimTraffic/is/SimTraffic/Herramientas/HCrearVia.java
@@ -6,7 +6,7 @@ import is.SimTraffic.IModelo;
 import is.SimTraffic.Mapa.Nodo;
 import is.SimTraffic.Mapa.Tramo;
 import is.SimTraffic.Mapa.Via;
-import is.SimTraffic.Mapa.TipoElemento.ITipoElemento;
+//import is.SimTraffic.Mapa.TipoElemento.ITipoElemento;
 
 public class HCrearVia implements IHerramienta {
 
@@ -15,14 +15,15 @@ public class HCrearVia implements IHerramienta {
 	ArrayList<Nodo> nodos,nodosGuardados;
 	ArrayList<Tramo> tramos,tramosGuardados;
 	
-	public HCrearVia(String nombre,ArrayList<Tramo> Lista,ITipoElemento tipoVia){
+	/**public HCrearVia(String nombre,ArrayList<Tramo> Lista,ITipoElemento tipoVia){
 		
 		via = new Via();
 		via.setNombre(nombre);
 		via.setTramos(Lista);
 		via.setTipo(tipoVia);
+		tramos = Lista;
 		
-	}
+	}*/
 	public HCrearVia(ArrayList<Nodo> ListaNodos,Via via){
 		this.nodos=ListaNodos;
 		this.via= via;
@@ -30,14 +31,16 @@ public class HCrearVia implements IHerramienta {
 	}
 	
 	public int hacer(IModelo modelo) {
-		nodosGuardados=nodos;
-		tramosGuardados =tramos;
+		nodosGuardados = new ArrayList<Nodo>();
 		while (!nodos.isEmpty()){
 			modelo.getMapa().insertar(nodos.get(0));
+			nodosGuardados.add(nodos.get(0));
 			nodos.remove(0);
 		}
+		tramosGuardados = new ArrayList<Tramo>();
 		while (!tramos.isEmpty()){
 			modelo.getMapa().insertar(tramos.get(0));
+			tramosGuardados.add(tramos.get(0));
 			tramos.remove(0);
 		}
 		modelo.getMapa().insertar(via);
@@ -45,14 +48,13 @@ public class HCrearVia implements IHerramienta {
 	}
 
 	public int deshacer(IModelo modelo) {
-		while (!nodos.isEmpty()){
-			nodosGuardados.get(0);
-			modelo.getMapa().eliminar(nodos.get(0));
-			nodosGuardados.remove(0);
-		}
 		while (!tramosGuardados.isEmpty()){
 			modelo.getMapa().eliminar(tramosGuardados.get(0));
 			tramosGuardados.remove(0);
+		}
+		while (!nodosGuardados.isEmpty()){
+			modelo.getMapa().eliminar(nodosGuardados.get(0));
+			nodosGuardados.remove(0);
 		}
 		modelo.getMapa().eliminar(via);
 		return 0;
