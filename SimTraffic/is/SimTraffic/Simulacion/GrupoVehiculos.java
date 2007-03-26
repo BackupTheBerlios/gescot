50603d1422167318ab8517fb9574eb07de87836b
emarchiori <emarchiori>
2007-03-26 19:31:28 +0000

    avances en el equema de la simulacion

--- SimTraffic/is/SimTraffic/Simulacion/GrupoVehiculos.java
+++ SimTraffic/is/SimTraffic/Simulacion/GrupoVehiculos.java
@@ -6,6 +6,13 @@ package is.SimTraffic.Simulacion;
 import java.util.ArrayList;
 
 /**
+ * Clase que procesa un grupo de vehiculos.<p>
+ * Esta clase extiende la clase Thread, permitiendo procear un grupo de vehiculos en paralelo
+ * con otro. Se utiliza esta estructura por cuestiones de eficiencia. El nro de vehiculos se
+ * define como una constante, establecida por experimentación en un valor óptimo.<br>
+ * Esta clase crea su propia instancia de la inteligencia, donde se realiza el procesamiento
+ * real de los vehiculos considerando su entorno.
+ * 
  * @author Grupo ISTrafico
  * 
  */
@@ -30,8 +37,23 @@ public class GrupoVehiculos extends Thread {
 	 */
 	private boolean termino = false;
 
-	public GrupoVehiculos(ArrayList<Vehiculo> vehiculos, int indice, Simulacion sim) {
-		lista = new Vehiculo[10];
+	/**
+	 * Método constructor de la clase.
+	 * <p>
+	 * Este método crea la "memoria" que procesara los coches e inicializa la
+	 * lista de vehiculos.
+	 * 
+	 * @param vehiculos
+	 *            ArrayList con todos los vehiculos de la simulación
+	 * @param indice
+	 *            Indice del primer vehiculo en el ArrayList perteneciente al
+	 *            grupo
+	 * @param sim
+	 *            Simulacion que se esta ejecutando
+	 */
+	public GrupoVehiculos(ArrayList<Vehiculo> vehiculos, int indice,
+			Simulacion sim) {
+		lista = new Vehiculo[nroVehiculos];
 		for (int i = indice; i < indice + nroVehiculos; i++) {
 			lista[i - indice] = vehiculos.get(i);
 		}
@@ -40,12 +62,16 @@ public class GrupoVehiculos extends Thread {
 
 	public void run() {
 		while (!termino) {
+			intel.actualizarInteligencia();
 			for (int i = 0; i < nroVehiculos; i++)
 				intel.procesar(lista[i]);
 			// TODO
 		}
 	}
 
+	/**
+	 * Método que finaliza la ejecución del thread.
+	 */
 	synchronized public void terminar() {
 		termino = true;
 	}
--- SimTraffic/is/SimTraffic/Simulacion/Inteligencia.java
+++ SimTraffic/is/SimTraffic/Simulacion/Inteligencia.java
@@ -6,14 +6,107 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class Inteligencia {
-	
+
+	/**
+	 * Almacena una referencia a la simulación actual
+	 */
 	private Simulacion sim;
-	
-	public Inteligencia (Simulacion sim) {
+
+	/**
+	 * Almacena los parametros de la simulación actual
+	 */
+	private ParametrosSimulacion param;
+
+	/**
+	 * Alamacena la relación entre tramos y vehiculos en un determinado momento.
+	 */
+	private Hashtable<Tramo, ArrayList<Vehiculo>> tabla;
+
+	public Inteligencia(Simulacion sim) {
 		this.sim = sim;
+		tabla = sim.getTabla();
+		actualizarInteligencia();
 	}
-	
-	synchronized public void procesar(Vehiculo vehiculo) {
+
+	/**
+	 * Método que coge del modelo los parametros que puedan haber variado.
+	 */
+	public void actualizarInteligencia() {
+		param = sim.getParam();
+	}
+
+	/**
+	 * Método que procesa el vehiculo.
+	 * <p>
+	 * Este método recurre a otros para procesar el vehiculo, según sus valores,
+	 * su posicion, la de los otros y su recorrido. Este método actualizara
+	 * finalmente resultará en la actualización de la posición y velocidad del
+	 * coche.
+	 * 
+	 * @param vehiculo
+	 */
+	public void procesar(Vehiculo vehiculo) {
+		// TODO
+		if (controlarFinTramo(vehiculo))
+			procesarTramoSiguiente(vehiculo);
+
+		controlarCochesDelante(vehiculo);
+
+		recalcularAceleracion(vehiculo);
+
+		recalcularVelocidadYPosicion(vehiculo);
+	}
+
+	/**
+	 * Método que controla que el coche no este llegando a fin de tramo.
+	 * <p>
+	 * (Explicar implmentacion)
+	 * 
+	 * @return booleano que sera verdadero si ya se llego al fin del tramo
+	 */
+	private boolean controlarFinTramo(Vehiculo vehiculo) {
+		// TODO
+		return false;
+	}
+
+	/**
+	 * Método para cambiar de tramo una vez que se llega al final del actual.
+	 * <p>
+	 * (Explicar implementacion)
+	 */
+	private void procesarTramoSiguiente(Vehiculo vehiculo) {
+		// TODO no olvidarse de actualizar la tabla de tramos y vehiculos
+	}
+
+	/**
+	 * Método para controlar si hay coches delante.
+	 * <p>
+	 * (Explicar implementacion)
+	 * 
+	 * @return Double que será la velocidad del coche de delante si hay uno cera
+	 *         y 0 en otro caso
+	 */
+	private double controlarCochesDelante(Vehiculo vehiculo) {
+		// TODO utilizar "tabla" para solo controlar entre los coches del tramo
+		return 0;
+	}
+
+	/**
+	 * Método para recalcular la acelearción de acuerdo a las condiciones dadas.
+	 * <p>
+	 * (Explicar implmentacion)
+	 */
+	private void recalcularAceleracion(Vehiculo vehiculo) {
+		// TODO
+	}
+
+	/**
+	 * Método para recalcular la velocidad a la que circula el vehiculo y su
+	 * posicion.
+	 * <p>
+	 * (Explicar implementacion)
+	 */
+	private void recalcularVelocidadYPosicion(Vehiculo vehiculo) {
 		// TODO
 	}
 }
