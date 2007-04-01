888201b2dd0d7e8b5c618ef2cf56ca82c5b76bd9
pablof <pablof>
2007-04-01 11:36:56 +0000

    Traspasado código a HPegar

--- SimTraffic/is/SimTraffic/Vista/EscuchasRaton/MLPegar.java
+++ SimTraffic/is/SimTraffic/Vista/EscuchasRaton/MLPegar.java
@@ -3,11 +3,14 @@ package is.SimTraffic.Vista.EscuchasRaton;
 import is.SimTraffic.IControlador;
 import is.SimTraffic.IModelo;
 import is.SimTraffic.Vista.PanelMapa;
+import is.SimTraffic.Herramientas.HPegar;
 import is.SimTraffic.Mapa.Nodo;
 import is.SimTraffic.Mapa.Tramo;
 import is.SimTraffic.Mapa.Posicion;
 
+import java.awt.Point;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
 
 /**
  * 
@@ -26,52 +29,12 @@ public class MLPegar extends EscuchaRaton {
 		// TODO Auto-generated method stub
 		double x = panel.lon_RepAMapa(arg0.getX());
 		double y = panel.lat_RepAMapa(arg0.getY());
-		double difX = x - modelo.getMapa().getNodoReferenciaPortapapeles().getPos().getLon();
-		double difY = y - modelo.getMapa().getNodoReferenciaPortapapeles().getPos().getLat();
-
-		//for (int i=0; i<modelo.getMapa().getPortapapeles().getNodosSeleccionados().size(); i++) {
-		for (int i=0; i<modelo.getMapa().getPortapapeles().getTramosSeleccionados().size(); i++) {
-			Tramo tramoTemp = modelo.getMapa().getPortapapeles().getTramosSeleccionados().get(i);			
-			Nodo nodoInicial = tramoTemp.getNodoInicial();
-			Nodo nodoFinal = tramoTemp.getNodoFinal();
-			Nodo nodoInicialTemp = new Nodo (nodoInicial.getPos().clone());
-			nodoInicialTemp.setPos(new Posicion(nodoInicialTemp.getPos().getLat()+difY,nodoInicialTemp.getPos().getLon()+difX));
-			Nodo nodoInicialMapa = modelo.getMapa().existeNodo(nodoInicialTemp);
-			if (nodoInicialMapa==null) {
-				nodoInicialMapa = nodoInicialTemp;
-				modelo.getMapa().insertar(nodoInicialMapa);
-			}
-			
-
-			Nodo nodoFinalTemp = new Nodo (nodoFinal.getPos().clone());
-			nodoFinalTemp.setPos(new Posicion(nodoFinalTemp.getPos().getLat()+difY,nodoFinalTemp.getPos().getLon()+difX));
-			Nodo nodoFinalMapa = modelo.getMapa().existeNodo(nodoFinalTemp);
-			if (nodoFinalMapa==null) {
-				nodoFinalMapa = nodoFinalTemp;
-				modelo.getMapa().insertar(nodoFinalMapa);
-			}
-			
-			
-			Tramo tramoMapa = tramoTemp.pseudoClone(nodoInicialMapa,nodoFinalMapa);
-			nodoInicialMapa.añadirTramo(tramoMapa);
-			nodoFinalMapa.añadirTramo(tramoMapa);
-			modelo.getMapa().insertar(tramoMapa);
-		}
-		
-		for (int i=0; i<modelo.getMapa().getPortapapeles().getNodosSeleccionados().size(); i++) {
-			Nodo nodoTemp = modelo.getMapa().getPortapapeles().getNodosSeleccionados().get(i);
-			Nodo nodoMapa = nodoTemp.pseudoClone();
-			nodoMapa.setPos(new Posicion(nodoMapa.getPos().getLat()+difY,nodoMapa.getPos().getLon()+difX));
-			if (modelo.getMapa().existeNodo(nodoMapa)==null) {
-				//Nodo nodoMapa = nodoTemp.pseudoClone();
-				modelo.getMapa().insertar(nodoMapa);
-				//nodoMapa.setPos(new Posicion(nodoMapa.getPos().getLat()+difY,nodoMapa.getPos().getLon()+difX));
-			}
-				
-		}
-		
+		Point2D puntoPegar = new Point2D.Double(x,y);
+		HPegar herramientaPegar = new HPegar (modelo.getMapa().getSeleccion().getNodosSeleccionados(),
+			modelo.getMapa().getSeleccion().getTramosSeleccionados(),puntoPegar);
+		controlador.herramienta(herramientaPegar);
 		panel.setRecrear(true);
-		panel.repaint();
+		panel.repaint();		
 	}
 
 
