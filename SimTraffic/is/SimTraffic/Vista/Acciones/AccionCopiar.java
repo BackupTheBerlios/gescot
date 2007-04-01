c8ff4f905d380a1123088022848770232e6b7316
pablof <pablof>
2007-04-01 11:35:10 +0000

    Modificado. Se traslada la operacion a HCopiar

--- SimTraffic/is/SimTraffic/Vista/Acciones/AccionCopiar.java
+++ SimTraffic/is/SimTraffic/Vista/Acciones/AccionCopiar.java
@@ -5,6 +5,7 @@ package is.SimTraffic.Vista.Acciones;
 
 import is.SimTraffic.IControlador;
 import is.SimTraffic.IModelo;
+import is.SimTraffic.Herramientas.HCopiar;
 import is.SimTraffic.Mapa.Nodo;
 import is.SimTraffic.Mapa.Posicion;
 import is.SimTraffic.Mapa.Seleccion;
@@ -48,49 +49,12 @@ public class AccionCopiar implements ActionListener {
 	
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		panel.setFocusable(true);		
-		if (modelo.getMapa().getSeleccion()!=null) {
-			modelo.getMapa().setPortapapeles(new Seleccion());
-			for (int j=0; j<modelo.getMapa().getSeleccion().getTramosSeleccionados().size(); j++) {
-				Tramo tramoMapa = modelo.getMapa().getSeleccion().
-										getTramosSeleccionados().get(j);
-								
-				Nodo nodoInicialEnPortapapeles = modelo.getMapa().getPortapapeles().existeNodo(tramoMapa.getNodoInicial());
-				if (nodoInicialEnPortapapeles==null)
-					nodoInicialEnPortapapeles = tramoMapa.getNodoInicial().pseudoClone();
-				
-				Nodo nodoFinalEnPortapapeles = modelo.getMapa().getPortapapeles().existeNodo(tramoMapa.getNodoFinal());
-				if (nodoFinalEnPortapapeles==null)
-					nodoFinalEnPortapapeles = tramoMapa.getNodoFinal().pseudoClone();
-				
-				modelo.getMapa().getPortapapeles().añadirNodo(nodoInicialEnPortapapeles);
-				modelo.getMapa().getPortapapeles().añadirNodo(nodoFinalEnPortapapeles);
-				
-				Tramo tramoPortapapeles = tramoMapa.pseudoClone(nodoInicialEnPortapapeles,nodoFinalEnPortapapeles);
-				modelo.getMapa().getPortapapeles().añadirTramo(tramoPortapapeles);
-				nodoInicialEnPortapapeles.añadirTramo(tramoPortapapeles);
-				nodoFinalEnPortapapeles.añadirTramo(tramoPortapapeles);									
-			}
-			for (int i=0; i<modelo.getMapa().getSeleccion().getNodosSeleccionados().size(); i++) {
-				Nodo nodoTemp = modelo.getMapa().getSeleccion().getNodosSeleccionados().get(i);
-				Nodo nodoPortapapeles = modelo.getMapa().getPortapapeles().existeNodo(nodoTemp);
-				if (nodoPortapapeles==null) {
-					nodoPortapapeles = nodoTemp.pseudoClone();
-					modelo.getMapa().getPortapapeles().añadirNodo(nodoPortapapeles);
-				}
-			}
-		}
-		Posicion posMinima= new Posicion(Double.MAX_VALUE,Double.MAX_VALUE);
-		for (int i=0; i<modelo.getMapa().getPortapapeles().getNodosSeleccionados().size();i++){
-			Nodo nodoTemp = modelo.getMapa().getPortapapeles().getNodosSeleccionados().get(i);
-			if (nodoTemp.getPos().getLon()<=posMinima.getLon()) {
-				if (nodoTemp.getPos().getLat()<=posMinima.getLat()) {
-					posMinima = nodoTemp.getPos();
-					modelo.getMapa().setNodoReferenciaPortapapeles(nodoTemp);
-				}
-			}
-		}
-
+		panel.setFocusable(true);
+		HCopiar herramientaCopiar = new HCopiar (modelo.getMapa().getSeleccion().getNodosSeleccionados(),
+				modelo.getMapa().getSeleccion().getTramosSeleccionados());
+		controlador.herramienta(herramientaCopiar);
+		
+		
 	}
 
 }
