85ac0fac74341e5b144539c422982a4931ab4bc2
namaja <namaja>
2007-04-14 15:50:40 +0000

    Creacion de JUNIT de TipoNodoAmenity

--- SimTraffic/is/SimTraffic/jUnit/Mapa/TipoElemento/TipoNodoAmenityTest.java
+++ SimTraffic/is/SimTraffic/jUnit/Mapa/TipoElemento/TipoNodoAmenityTest.java
@@ -13,9 +13,35 @@ public class TipoNodoAmenityTest extends TestCase {
 		if (tipo1 != tipo1)
 			fail("Tipo deberia ser igual a si misma");
 		if (!tipo1.equals(tipo2))
-			fail("Posciones con las mismas coordenadas deberian ser iguales");			
+			fail("Tipos con el mismo tipo deberian ser iguales");			
 		if (tipo1.equals(tipo3))
-			fail("Posiciones con coordenadas diferentes deberían ser diferentes");	
+			fail("Tipos con distintos tipos deberían ser diferentes");	
 	}
+	
+	public void testtraduciraOSMOSM()
+	{
+		TipoNodoAmenity tipo1 = new TipoNodoAmenity("Parking");
+		TipoNodoAmenity tipo2= new TipoNodoAmenity("Parking");
+		TipoNodoAmenity tipo3= new TipoNodoAmenity("Gasolinera");
+		if (tipo1.traduciraOSM(tipo1.getValorTipo())!=tipo1.traduciraOSM(tipo1.getValorTipo()))
+			fail("Tipo deberia tener la misma traduccion");
+		if (tipo1.traduciraOSM(tipo1.getValorTipo())!=tipo2.traduciraOSM(tipo2.getValorTipo()))
+			fail("Tipo deberia tener la misma traduccion que un tipo igual a este");
+		if (tipo1.traduciraOSM(tipo1.getValorTipo())==tipo3.traduciraOSM(tipo3.getValorTipo()))
+			fail("Los tipos deberian tener una traduccion distinta");
+	}
+	public void testtraduciraCastellano()
+	{
+		TipoNodoAmenity tipo1 = new TipoNodoAmenity("Parking");
+		TipoNodoAmenity tipo2= new TipoNodoAmenity("Parking");
+		TipoNodoAmenity tipo3= new TipoNodoAmenity("Gasolinera");
+		if (tipo1.traduciraCastellano(tipo1.traduciraOSM(tipo1.getValorTipo()))!=tipo1.traduciraCastellano(tipo1.traduciraOSM(tipo1.getValorTipo())))
+			fail("Tipo deberia tener la misma traduccion");
+		if (tipo1.traduciraCastellano(tipo1.traduciraOSM(tipo1.getValorTipo()))!=tipo2.traduciraCastellano(tipo2.traduciraOSM(tipo2.getValorTipo())))
+			fail("Tipo deberia tener la misma traduccion que un tipo igual a este");
+		if (tipo1.traduciraCastellano(tipo1.traduciraOSM(tipo1.getValorTipo()))==tipo3.traduciraCastellano(tipo3.traduciraOSM(tipo3.getValorTipo())))
+			fail("Los tipos deberian tener una traduccion distinta");
+	}
+	
 
 }
