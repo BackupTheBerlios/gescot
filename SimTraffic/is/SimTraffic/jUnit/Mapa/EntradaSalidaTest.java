808227b0512dfda3dbede50ac3dedc7ea5d817ef
namaja <namaja>
2007-04-19 12:13:42 +0000

    inclusion de metodo equals

---
+++ SimTraffic/is/SimTraffic/jUnit/Mapa/EntradaSalidaTest.java
@@ -0,0 +1,91 @@
+package is.SimTraffic.jUnit.Mapa;
+
+import is.SimTraffic.Mapa.EntradaSalida;
+import is.SimTraffic.Mapa.Nodo;
+import is.SimTraffic.Mapa.Posicion;
+import junit.framework.TestCase;
+
+public class EntradaSalidaTest extends TestCase {
+	
+	public void testEquals()
+	{
+		int entrada1 []= new int [12];
+		entrada1[0]=5;
+		entrada1[1]=4;
+		entrada1[2]=6;
+		entrada1[3]=7;
+		entrada1[4]=1;
+		entrada1[5]=2;
+		entrada1[6]=9;
+		entrada1[7]=3;
+		entrada1[8]=5;
+		entrada1[9]=6;
+		entrada1[10]=5;
+		entrada1[11]=10;
+		int entrada2 []= new int [12];
+		entrada2[0]=4;
+		entrada2[1]=5;
+		entrada2[2]=6;
+		entrada2[3]=7;
+		entrada2[4]=8;
+		entrada2[5]=9;
+		entrada2[6]=10;
+		entrada2[7]=11;
+		entrada2[8]=12;
+		entrada2[9]=13;
+		entrada2[10]=14;
+		entrada2[11]=15;
+		EntradaSalida es1= new EntradaSalida(5,2,entrada1,entrada2);
+		EntradaSalida es2= new EntradaSalida(5,2,entrada1,entrada2);
+		
+		if (es1 != es1)
+			fail("EntradaSalida deberia ser igual a si mismo");
+		if (!es1.equals(es2))
+			fail("EntradaSalida con los mismos valores deberian ser iguales");
+		EntradaSalida es3= new EntradaSalida(5,2,entrada2,entrada2);
+		if (es1.equals(es3))
+			fail("Nodos tendrian que ser diferentes");	
+	}
+	
+	public void testtransformaOSM()
+	{
+	
+	int entrada1 []= new int [12];
+	entrada1[0]=5;
+	entrada1[1]=4;
+	entrada1[2]=6;
+	entrada1[3]=7;
+	entrada1[4]=1;
+	entrada1[5]=2;
+	entrada1[6]=9;
+	entrada1[7]=3;
+	entrada1[8]=5;
+	entrada1[9]=6;
+	entrada1[10]=5;
+	entrada1[11]=10;
+	int entrada2 []= new int [12];
+	entrada2[0]=4;
+	entrada2[1]=5;
+	entrada2[2]=6;
+	entrada2[3]=7;
+	entrada2[4]=8;
+	entrada2[5]=9;
+	entrada2[6]=10;
+	entrada2[7]=11;
+	entrada2[8]=12;
+	entrada2[9]=13;
+	entrada2[10]=14;
+	entrada2[11]=15;
+	EntradaSalida es1= new EntradaSalida(5,2,entrada1,entrada2);
+	EntradaSalida es2= new EntradaSalida(5,2,entrada1,entrada2);
+	if (es1.transformaOSM()!=es1.transformaOSM())
+		fail("La traducion OSM  de si mi mismo deberia ser igual");
+	if (es1.transformaOSM()!=es2.transformaOSM())
+		fail("La traducion OSM de los mismos datos deberia ser igual");
+	EntradaSalida es3= new EntradaSalida(5,2,entrada2,entrada1);
+	if (es1.transformaOSM()==es3.transformaOSM())
+		fail("La traducion OSM de distintos datos deberia ser distinta");
+	}
+	
+
+}
