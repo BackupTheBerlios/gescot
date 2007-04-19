0ada968750273380016c86095ab2ee256645b070
carlostb <carlostb>
2007-04-19 11:36:53 +0000

    tiempo para logs en doble cifra para horas, minutos y segundos

--- SimTraffic/is/SimTraffic/Utils/Tiempo.java
+++ SimTraffic/is/SimTraffic/Utils/Tiempo.java
@@ -13,6 +13,14 @@ public class Tiempo {
 	    int sec = cal.get(Calendar.SECOND);             // 0..59
 	    int ms = cal.get(Calendar.MILLISECOND);         // 0..999
 	    int ampm = cal.get(Calendar.AM_PM);             // 0=AM, 1=PM
-	    return "["+hour24+":"+min+":"+sec+"]";
+	    
+	    return "["+convierte(hour24)+":"+convierte(min)+":"+convierte(sec)+"]";
+	}
+	
+	private static String convierte(int valor){
+		if (valor <10){
+			return "0"+valor;
+		}
+		else return ""+valor;
 	}
 }
