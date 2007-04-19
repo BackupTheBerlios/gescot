9cb6cd3462c880cc789ed64375bc186d98319c57
smoken <smoken>
2007-04-19 18:37:33 +0000

    Accion utilizada en el Panel Nodo

---
+++ SimTraffic/is/SimTraffic/Vista/Acciones/PanelNodo/AccionCrearSemaforo.java
@@ -0,0 +1,32 @@
+package is.SimTraffic.Vista.Acciones.PanelNodo;
+
+import is.SimTraffic.Mapa.Nodo;
+import is.SimTraffic.Mapa.Señal;
+import is.SimTraffic.Mapa.Señales.Semaforo;
+import is.SimTraffic.Vista.PanelNodo;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class AccionCrearSemaforo implements ActionListener {
+	private Nodo nodo;
+	private JFrame ventanaPadre;
+	
+	public AccionCrearSemaforo(Nodo nodo, JFrame ventanaPadre){
+		this.nodo = nodo;
+		this.ventanaPadre = ventanaPadre;
+		
+	}
+	
+	public void actionPerformed(ActionEvent arg0) {
+		Señal semaforo = new Semaforo(nodo);
+		nodo.setSeñal(semaforo);
+		//Actualizamos la vista de los semáforos.
+		((PanelNodo)ventanaPadre).creaPanelSemaforos();
+		((PanelNodo)ventanaPadre).daFocoAPanelSemaforo();
+		
+	}
+
+}
