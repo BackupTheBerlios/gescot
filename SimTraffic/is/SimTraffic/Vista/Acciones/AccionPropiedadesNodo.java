df600f7ba7f2d985ec45a29e79943ad6ff77b87d
javi_sna <javi_sna>
2007-04-20 12:17:50 +0000

    Añadido AccionPorpiedadesNodo para ver las propiedades del nodo con el boton derecho

---
+++ SimTraffic/is/SimTraffic/Vista/Acciones/AccionPropiedadesNodo.java
@@ -0,0 +1,31 @@
+package is.SimTraffic.Vista.Acciones;
+
+import is.SimTraffic.IControlador;
+import is.SimTraffic.IModelo;
+import is.SimTraffic.Mapa.Nodo;
+import is.SimTraffic.Mapa.Tramo;
+import is.SimTraffic.Vista.PanelMapa;
+import is.SimTraffic.Vista.PanelNodo;
+
+import java.awt.event.ActionEvent;
+
+public class AccionPropiedadesNodo extends AccionesPopUpMenu{
+	public AccionPropiedadesNodo(IModelo modelo, IControlador controlador,
+			PanelMapa panel_mapa) {
+		super(modelo, controlador, panel_mapa);
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		Nodo nodoAux=this.buscarNodo(panel_mapa.getPosEx(), panel_mapa
+				.getPosEy());
+		if (nodoAux != null) {
+			//JFrame ventanaNodo = new JFrame();
+			PanelNodo panelNod = new PanelNodo(nodoAux, panel_mapa);
+			panelNod.setTitle("Propiedades del Nodo");
+			panelNod.setBounds(80, 120, 500, 600);
+			panelNod.setVisible(true);
+		}
+	}
+
+}
+
