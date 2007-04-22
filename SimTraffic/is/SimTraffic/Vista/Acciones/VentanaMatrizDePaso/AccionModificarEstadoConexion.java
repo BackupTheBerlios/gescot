2a1e25882f8e0d923930ffc3ad96d67f00b6e07b
smoken <smoken>
2007-04-22 12:31:25 +0000

    Oyentes (uno único), de la ventana matriz de paso.

---
+++ SimTraffic/is/SimTraffic/Vista/Acciones/VentanaMatrizDePaso/AccionModificarEstadoConexion.java
@@ -0,0 +1,42 @@
+package is.SimTraffic.Vista.Acciones.VentanaMatrizDePaso;
+
+import is.SimTraffic.Mapa.Nodo;
+import is.SimTraffic.Mapa.Señales.Semaforo;
+import is.SimTraffic.Vista.VentanaMatrizPaso.BotonDeConexion;
+import is.SimTraffic.Vista.VentanaMatrizPaso.VentanaMatrizDePaso;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class AccionModificarEstadoConexion implements ActionListener {
+
+	JFrame ventanaPadre;
+	
+	public AccionModificarEstadoConexion(JFrame ventanaPadre) {
+		this.ventanaPadre = ventanaPadre;
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		BotonDeConexion botonFuente = ((BotonDeConexion)arg0.getSource());
+		int tramoOrigen = botonFuente.getTramoOrigen();
+		int tramoDestino = botonFuente.getTramoDestino();
+		int numIntervalo = ((VentanaMatrizDePaso)ventanaPadre).getNumIntervalo();
+		
+		Nodo nodo = ((VentanaMatrizDePaso)ventanaPadre).getNodo();
+		((Semaforo)nodo.getSeñal()).getListaIntervalos().get(numIntervalo).getMatrizDePaso().modificarPaso(tramoOrigen,tramoDestino);
+		
+		
+		//Por último, pintamos el botón del color correspondiente.
+		if (((Semaforo)nodo.getSeñal()).getListaIntervalos().get(numIntervalo).getMatrizDePaso().sePuedePasar(tramoOrigen,tramoDestino) == 0){
+			botonFuente.setBackground(Color.RED);
+		} else if (((Semaforo)nodo.getSeñal()).getListaIntervalos().get(numIntervalo).getMatrizDePaso().sePuedePasar(tramoOrigen,tramoDestino) == 1){
+			botonFuente.setBackground(Color.GREEN);
+		}
+	}
+	
+	
+
+}
