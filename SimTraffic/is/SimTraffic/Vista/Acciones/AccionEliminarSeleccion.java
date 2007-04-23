1c5327798ee8f52ba3bb326a13f81701154f5b77
pablof <pablof>
2007-04-23 15:42:05 +0000

    Accion para eliminar una seleccion

--- SimTraffic/is/SimTraffic/Vista/Acciones/AccionEliminarSeleccion.java
+++ SimTraffic/is/SimTraffic/Vista/Acciones/AccionEliminarSeleccion.java
@@ -4,17 +4,20 @@ import java.awt.event.ActionEvent;
 
 import is.SimTraffic.IControlador;
 import is.SimTraffic.IModelo;
+import is.SimTraffic.Herramientas.HEliminarNodo;
+import is.SimTraffic.Herramientas.HEliminarSeleccion;
+import is.SimTraffic.Mapa.Nodo;
 import is.SimTraffic.Vista.PanelMapa;
 
 /**
  * Clase que extiende ActionListener para permitir a los botones o elementos del
- * menú eliminar un nodo del mapa.
+ * menú eliminar los elementos seleccionados.
  * <p>
  * <p>
  * Esta clase tomara el modelo, el controlador y el panel con la representación
  * del mapa como parámetros en su construcutor y lo almacenará para que cuando
- * se ejecute la acción poder llamar a la herramienta HEliminarNodo encargada
- * eliminar efectivametne el nodo del mapa.
+ * se ejecute la acción poder llamar a la herramienta HEliminarSeleccion encargada
+ * de eliminar la selección de elementos del mapa.
  * 
  * @author Grupo ISTrafico
  * 
@@ -29,12 +32,15 @@ public class AccionEliminarSeleccion extends AccionesPopUpMenu {
 	}
 
 	public void actionPerformed(ActionEvent arg0) {
-		if (modelo.getMapa().getSeleccion().esVacia()) {
-			//panel_mapa.getMenuEmergente().setVisible(false);
-			//controlador.herramienta(new HEliminarSeleccion());
+		if (!modelo.getMapa().getSeleccion().esVacia()) {			
+			panel_mapa.getMenuEmergente().setVisible(false);
+			controlador.herramienta(new HEliminarSeleccion(
+					modelo.getMapa().getSeleccion().getNodosSeleccionados(),
+					modelo.getMapa().getSeleccion().getTramosSeleccionados()));
+			modelo.getMapa().limpiaSeleccion();
+			panel_mapa.sugerir(null);
 			panel_mapa.recrearMapa();
 			panel_mapa.repaint();
 		}
 	}
-
 }
