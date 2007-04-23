91271a4bd51ddbd3c1f2a704b8b8189cd4277028
pablof <pablof>
2007-04-23 15:54:14 +0000

    Ahora se selecciona el nodo al hacer boton derecho

--- SimTraffic/is/SimTraffic/Vista/EscuchasRaton/MLSeleccionaNodoBDerecho.java
+++ SimTraffic/is/SimTraffic/Vista/EscuchasRaton/MLSeleccionaNodoBDerecho.java
@@ -11,25 +11,23 @@ import java.awt.event.MouseEvent;
 
 public class MLSeleccionaNodoBDerecho extends EscuchaRaton {
 	
-	private Nodo seleccionado;
-	private boolean drag;
+	private Nodo seleccionado;	
+	
 	public MLSeleccionaNodoBDerecho(IModelo modelo, IControlador controlador, PanelMapa panel){
 		super(modelo, controlador, panel);
-		panel.setFocusable(true);
-		drag = false;
+		panel.setFocusable(true);		
 	}
 
 		public void mouseClicked(MouseEvent arg0) {
 			if(arg0.isPopupTrigger()){
 				seleccionado = buscarNodo(arg0.getX(), arg0.getY());
 				if (seleccionado != null){
-					if (modelo.getMapa().getSeleccion().getNodosSeleccionados().contains(seleccionado)){
-						modelo.getMapa().getSeleccion().getNodosSeleccionados().remove(seleccionado);
-					} else {
+					if (modelo.getMapa().getSeleccion().esVacia()){
 						modelo.getMapa().getSeleccion().añadirNodo(seleccionado);
 					}
-					panel.setPosE(arg0.getX(), arg0.getY());
-				}
+				}				
+				panel.repaint();				
+				panel.setPosE(arg0.getX(), arg0.getY());
 			}
 		}
 
@@ -48,7 +46,6 @@ public class MLSeleccionaNodoBDerecho extends EscuchaRaton {
 		if(arg0.isPopupTrigger() && buscarNodo(arg0.getX(), arg0.getY())!= null){
 			panel.setPosE(arg0.getX(), arg0.getY());
 			panel.setPuntoInicial(arg0.getPoint());
-			panel.setModoSeleccion(true);
 			panel.getMenuEmergente().show(panel,arg0.getX(),arg0.getY());
 		}
 	}
@@ -58,18 +55,14 @@ public class MLSeleccionaNodoBDerecho extends EscuchaRaton {
 		if(arg0.isPopupTrigger() && buscarNodo(arg0.getX(), arg0.getY()) != null){
 			panel.setPosE(arg0.getX(), arg0.getY());
 			panel.setPuntoInicial(arg0.getPoint());
-			panel.setModoSeleccion(true);
+			seleccionado = buscarNodo(arg0.getX(), arg0.getY());			
+			if (seleccionado != null){
+				if (modelo.getMapa().getSeleccion().esVacia()){
+					modelo.getMapa().getSeleccion().añadirNodo(seleccionado);
+				}
+			}
 			panel.getMenuEmergente().show(panel,arg0.getX(),arg0.getY());
-			panel.setModoSeleccion(false);
 			panel.repaint();
-			
-			if (this.getModificadorDeTeclado() != 17 && drag)
-				this.modelo.getMapa().limpiaSeleccion();
-
-			if (drag)
-				panel.notificaSeleccion(0);
-
-			drag = false;
 		}
 	}
 
