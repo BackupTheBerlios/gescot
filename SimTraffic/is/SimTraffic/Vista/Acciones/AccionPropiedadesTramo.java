d96eb0563cd6a47cb042ee8c16f9167fbba7cf13
pablof <pablof>
2007-04-23 19:50:06 +0000

    Para mostrar propiedades del tramo con botón derecho

---
+++ SimTraffic/is/SimTraffic/Vista/Acciones/AccionPropiedadesTramo.java
@@ -0,0 +1,31 @@
+package is.SimTraffic.Vista.Acciones;
+
+import java.awt.event.ActionEvent;
+
+import is.SimTraffic.IControlador;
+import is.SimTraffic.IModelo;
+import is.SimTraffic.Mapa.Tramo;
+import is.SimTraffic.Vista.PanelMapa;
+import is.SimTraffic.Vista.PanelTramo;
+
+public class AccionPropiedadesTramo extends AccionesPopUpMenu{
+	
+	
+	public AccionPropiedadesTramo(IModelo modelo, IControlador controlador,
+			PanelMapa panel_mapa) {
+		super(modelo, controlador, panel_mapa);
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		Tramo tramoAux=this.buscarTramo(panel_mapa.getPosEx(), panel_mapa
+				.getPosEy());
+		if (tramoAux != null) {
+			//JFrame ventanaNodo = new JFrame();
+			PanelTramo panelTramo = new PanelTramo(tramoAux, panel_mapa,controlador);
+			panelTramo.setTitle("Propiedades del tramo");
+			panelTramo.setBounds(80, 120, 500, 600);
+			panelTramo.setVisible(true);
+		}
+	}
+
+}
