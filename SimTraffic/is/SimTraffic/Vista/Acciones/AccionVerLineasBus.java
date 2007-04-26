8fc8c176b62421226ad9a91173c4d401664eed4d
inigo <inigo>
2007-04-26 09:59:31 +0000

    Mejor Accion ver lineas bus

--- SimTraffic/is/SimTraffic/Vista/Acciones/AccionVerLineasBus.java
+++ SimTraffic/is/SimTraffic/Vista/Acciones/AccionVerLineasBus.java
@@ -2,6 +2,7 @@ package is.SimTraffic.Vista.Acciones;
 
 import is.SimTraffic.IControlador;
 import is.SimTraffic.IModelo;
+import is.SimTraffic.Mapa.LineaBus;
 import is.SimTraffic.Mapa.Tramo;
 import is.SimTraffic.Mapa.Via;
 
@@ -24,65 +25,96 @@ import javax.swing.border.Border;
 public class AccionVerLineasBus implements ActionListener{
 
 	IControlador controlador;
-
-	IModelo modelo;
-	
-	JFrame ventana;
-	
+	IModelo modelo;	
 	JPanel ventanaLineasBus;
+	JPanel panel_mapa;
+	List lineasBus;
 
-	public AccionVerLineasBus(IControlador controlador, IModelo modelo,JFrame ventana) {
+	public AccionVerLineasBus(IControlador controlador, IModelo modelo,JFrame ventana,JPanel panel_mapa) {
 		super();
 		this.controlador = controlador;
 		this.modelo = modelo;
-		this.ventana=ventana;
-		
+		this.panel_mapa=panel_mapa;
+		poneLineasEnLista();
 	}
 
 	public void actionPerformed(ActionEvent arg0) {
 		
-		 //Auxiliar
-		 JFrame ventana2 =new JFrame();
-		 ventana2.setSize(200,300);
-		 ventana2.setTitle("Lineas de Autobuses");
 		 
-		
-		
-		 //ventanaLineasBus = new JPanel();
-		 //this.ventana.getContentPane().add(ventanaLineasBus, BorderLayout.EAST);
-		 Point esquinaIzq=this.ventana.getLocationOnScreen();
-		 int XVentana=(int)esquinaIzq.getX()+this.ventana.getWidth()-323;
-		 int YVentana=(int)esquinaIzq.getY()+this.ventana.getHeight()-666;
-		 //ventanaLineasBus.setBounds(XVentana,YVentana,300,400);
-		 //ventanaLineasBus.setVisible(true);
-		 ventana2.add(poneLineasEnLista());//,BorderLayout.CENTER);
-		 ventana2.setVisible(true);
+		 ventanaLineasBus = new JPanel(new BorderLayout());
 		 
 		 
-		// Donde va a aparecer la ventana son las dos primera componentes
-		// El tamaño de la ventana son las dos ultimas componentes
-		 
+		 JLabel iconoBus = new JLabel((new ImageIcon(
+		 			"is\\SimTraffic\\Vista\\Imagenes\\autobus.jpg" )));
+		 JPanel imagenBus =new JPanel();
+		 imagenBus.setLayout(new BorderLayout());
+		 imagenBus.add(iconoBus);
+		 ventanaLineasBus.add(imagenBus,BorderLayout.WEST); 
+		 JScrollPane panel_lista =new JScrollPane(lineasBus);
+		 panel_lista.setBorder(BorderFactory.createEmptyBorder(20, 10, 150, 20));
+		 JPanel titulo =new JPanel();
+		 JLabel label=new JLabel("Lineas de autobus");
+		 label.setFont(new Font("serif", Font.BOLD, 15));
+		 titulo.add(label);
+		 ventanaLineasBus.add(titulo,BorderLayout.NORTH);
+		 ventanaLineasBus.add(panel_lista,BorderLayout.CENTER);
+		 ventanaLineasBus.setBorder(BorderFactory.createRaisedBevelBorder());
+		 JButton boton_salir= new JButton("Salir");
+		 boton_salir.addActionListener(new accionSalir());
+		 JButton boton_buscar=new JButton("Buscar Linea");
+		 boton_buscar.addActionListener(new accionBuscar());
+		 JPanel botones=new JPanel(new BorderLayout());
+		 botones.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		 botones.add(boton_buscar,BorderLayout.NORTH);
+		 botones.add(boton_salir,BorderLayout.SOUTH);
+		 ventanaLineasBus.add(botones,BorderLayout.SOUTH);
+		 panel_mapa.add(ventanaLineasBus,BorderLayout.WEST);
+		 panel_mapa.repaint();
 
 	}
 	
-	private JList poneLineasEnLista(){
-		ArrayList<Via> lista=modelo.getMapa().getLineasAutobuses();
-		DefaultListModel modelo = new DefaultListModel();
-		Iterator<Via> it = lista.iterator();
+	private void poneLineasEnLista(){
+		ArrayList<LineaBus> lista=modelo.getMapa().getLineasAutobuses();
+		 lineasBus = new List();
+		Iterator<LineaBus> it = lista.iterator();
 		while (it.hasNext()){
-		 modelo.addElement(it.next().getNombre());
+		 lineasBus.add(it.next().getNombre());
 		}
-		JList lineasBus = new JList(modelo);
-		Border blackline = BorderFactory.createLineBorder(Color.black);
-		lineasBus.setBorder(blackline);
-		//ListSelectionListener seleccion = new SeleccionarLista();
-		//lineasBus.addListSelectionListener();
-		return lineasBus;
 	}
 	
+	private class accionSalir implements ActionListener {
+
+		public void actionPerformed(ActionEvent arg0) {
+			panel_mapa.remove(ventanaLineasBus);
+			ventanaLineasBus=null;
+			panel_mapa.repaint();
+		}
+	}
 	
-	
-	
+	private class accionBuscar implements ActionListener {
+
+		public void actionPerformed(ActionEvent arg0) {
+			ArrayList<LineaBus> lista=modelo.getMapa().getLineasAutobuses();
+			if (!lista.isEmpty()){
+			LineaBus seleccionada = lista.get(lineasBus.getSelectedIndex());
+			Iterator tramos = seleccionada.getTramos().iterator();
+			Iterator paradas = seleccionada.getParadas().iterator();
+			paradas.next();
+			  while(tramos.hasNext()){
+			   Tramo aux = (Tramo)tramos.next();
+			   Boolean parada = (Boolean)paradas.next();
+			   //Añadimos cada uno de los tramos	
+			   modelo.getMapa().getSeleccion().añadirTramo(aux);
+			   //Añadimos cada uno de los nodos que une cada tramo de la via
+			   modelo.getMapa().getSeleccion().añadirNodo(aux.getNodoInicial());
+			   //Si hay una parada la añadimos a la seleccion
+			   if (parada)
+			   modelo.getMapa().getSeleccion().añadirNodo(aux.getNodoFinal());
+			 }
+			}
+		}
+	}
+
 	
 
 	
