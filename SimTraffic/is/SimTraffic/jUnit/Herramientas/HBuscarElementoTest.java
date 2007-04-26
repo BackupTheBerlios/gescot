92d292a51f5e8ed9bc18f301b58ea02cd8f3ff72
namaja <namaja>
2007-04-26 17:14:17 +0000

    añadido la pruba de deshacer y comentarios

--- SimTraffic/is/SimTraffic/jUnit/Herramientas/HBuscarElementoTest.java
+++ SimTraffic/is/SimTraffic/jUnit/Herramientas/HBuscarElementoTest.java
@@ -16,27 +16,55 @@ import java.util.ArrayList;
 
 import junit.framework.TestCase;
 
+/**
+ * Clase JUNIT para testear la clase BuscarElemento
+ * @author Nacho
+ *
+ */
 public class HBuscarElementoTest extends TestCase {
 	
-	HAsignarTramosAVia t;
-	Via v;
-	
+	/**
+	 * Atibuto que indica el nombre del nodo
+	 */
 	String nombre;
 	
+	/**
+	 * Atributo que indica el tipo a eliminar
+	 */
 	String eliminar;
 	
+	/**
+	 * Atributo Nodo que se añade al modelo
+	 */
 	Nodo nodo;
-	
+	/**
+	 * Atributo que es el modelo
+	 */
 	Modelo modelo;
-	
+	/**
+	 * Atributo que es un panel necesario para las pruebas
+	 */
 	PanelMapa panel;
 	
+	/**
+	 * Atributo con la herramienta que es la clase que vamos a probar
+	 */
 	HBuscarElemento herramienta;
 
+
+	/**
+	 * Constructor de la clase
+	 * @param name
+	 */
 	public HBuscarElementoTest(String name) {
 		super(name);
 	}
 
+	/**
+	 * Método de setUp de los atributos de prueba
+	 * Se construyen: un nodo, un modelo, un jpanel y una herramienta
+	 * la EntradaSalida
+	 */
 	protected void setUp() throws Exception {
 		super.setUp();
 		nodo = new Nodo(0, "nodo1",new Posicion(100,100),new TipoNodoLeisure("Marina"));
@@ -48,19 +76,30 @@ public class HBuscarElementoTest extends TestCase {
 		herramienta=new HBuscarElemento(eliminar,nombre,panel);
 	}
 	
+	/**
+	 * Metodo que comprueba el constructor de la clase HBuscarElemento
+	 */
 	public void testHBuscarElemento()
 	{
 		assertEquals(nombre,((HBuscarElemento)herramienta).getNombre());
 		assertEquals(eliminar,((HBuscarElemento)herramienta).getElementoACambiar());
 		assertEquals(panel,((HBuscarElemento)herramienta).getPanel());	
 	}
-	
-	public void testHacer() {
+	/**
+	 * Metodo que comprueba el metodo hacer de la clase HBuscarElemento
+	 */
+	public void testhacer() {
 		if (herramienta.hacer(modelo)==1)
 			fail("Deberia encontrar el nodo");
 		herramienta=new HBuscarElemento(eliminar,"nodo2",panel);
 		if (herramienta.hacer(modelo)!=1)
 			fail("No deberia encontrar el nodo");
+	}
+	/**
+	 * Metodo que comprueba el metodo deshacer de la clase HBuscarElemento
+	 */
+	public void testdeshacer() {
+		assertEquals(0,herramienta.deshacer(this.modelo));
 	}
 
 }
