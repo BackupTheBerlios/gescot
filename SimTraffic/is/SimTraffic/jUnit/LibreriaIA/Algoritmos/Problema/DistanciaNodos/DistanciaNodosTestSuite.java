5aad1e557a0d814a2aede3388a951a701deca5a9
jogan <jogan>
2007-05-04 12:43:55 +0000

    Más junit

---
+++ SimTraffic/is/SimTraffic/jUnit/LibreriaIA/Algoritmos/Problema/DistanciaNodos/DistanciaNodosTestSuite.java
@@ -0,0 +1,24 @@
+package is.SimTraffic.jUnit.LibreriaIA.Algoritmos.Problema.DistanciaNodos;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Clase de prueba que llama a todos los tests del paquete DistanciaNodos
+ * 
+ */
+public class DistanciaNodosTestSuite {
+
+	/**
+	 * Método suite que añade los tests de todas las clases de prueba
+	 * 
+	 */
+	public static Test suite() {
+		TestSuite suite = new TestSuite();
+		//suite.addTestSuite(EstadoDistanciaNodosTest.class);
+		//suite.addTestSuite(ExploraNodoTest.class);
+		//suite.addTestSuite(HeuristicaDistanciaLineaRectaTest.class);
+		//suite.addTestSuite(PrincipalDistanciaNodosTest.class);
+		return suite;
+	}
+}
