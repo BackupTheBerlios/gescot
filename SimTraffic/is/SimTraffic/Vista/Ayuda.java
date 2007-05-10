d6866699cbdf55f0cea7fe3aafab59bac6a6ddba
carlostb <carlostb>
2007-05-10 12:51:34 +0000

    Ayuda

--- SimTraffic/is/SimTraffic/Vista/Ayuda.java
+++ SimTraffic/is/SimTraffic/Vista/Ayuda.java
@@ -1,97 +1,100 @@
 package is.SimTraffic.Vista;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import java.io.*;
 
-import javax.swing.JButton;
-import javax.swing.JEditorPane;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.event.HyperlinkEvent;
-import javax.swing.event.HyperlinkListener;
+public class Ayuda extends JFrame implements HyperlinkListener, 
+                                               ActionListener {
+  public static void main(String[] args) {
+    if (args.length == 0)
+      new Ayuda("http://www.apl.jhu.edu/~hall/");
+    else
+      new Ayuda(args[0]);
+  }
 
-/**
- * 
- * @author Grupo ISTrafico
- *
- */
-public class Ayuda extends JFrame implements HyperlinkListener,ActionListener{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -4996009540610924374L;
-	
-	
-		
-	/**
-	 * Campo de texto para mostrar los eventos
-	 */
-	private JEditorPane htmlPane;
-	private JScrollPane scrollPane;
-	
-	Ayuda() {
-		setTitle("Ayuda de SimTraffic");
-		setVisible(true);
-		setLayout(new BorderLayout());
-		
-		
-		String url = "http://simtraffic.helker.com/Manual2/manual.html";
-		try {
-		  htmlPane = new JEditorPane(url);
-		} catch(IOException ioe) {
-		  System.err.println("Error displaying " + url);
-		}
-		
-		htmlPane.setEditable(false);
-		scrollPane = new JScrollPane (htmlPane, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED); 
-		add(scrollPane, BorderLayout.CENTER);
-		
-		htmlPane.setContentType("text/html"); 
-		
-		
-		
-		
-		JButton aceptar = new JButton("Aceptar");
-		aceptar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				//Log.this.setVisible(false);
-				Ayuda.this.dispose();
-			}
-		}
-		);
-		add(aceptar, BorderLayout.SOUTH);
-		
-		this.setPreferredSize(new Dimension(640,480));
-		this.pack();
-		//this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		this.setResizable(true);
-		this.setLocation(50, 50);
-		//this.repaint();
-		
+  private JIconButton homeButton;
+  private JTextField urlField;
+  private JEditorPane htmlPane;
+  private String initialURL;
+
+  public Ayuda(String initialURL) {
+    super("Simple Swing Ayuda");
+    this.initialURL = initialURL;
+    
+    JPanel topPanel = new JPanel();
+    topPanel.setBackground(Color.lightGray);
+    homeButton = new JIconButton("home.gif");
+    homeButton.addActionListener(this);
+    JLabel urlLabel = new JLabel("URL:");
+    urlField = new JTextField(30);
+    urlField.setText(initialURL);
+    urlField.addActionListener(this);
+    topPanel.add(homeButton);
+    topPanel.add(urlLabel);
+    topPanel.add(urlField);
+    //getContentPane().add(topPanel, BorderLayout.NORTH);
+
+    try {
+        htmlPane = new JEditorPane(initialURL);
+        htmlPane.setEditable(false);
+        htmlPane.addHyperlinkListener(this);
+        JScrollPane scrollPane = new JScrollPane(htmlPane);
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+    } catch(IOException ioe) {
+       warnUser("Can't build HTML pane for " + initialURL 
+                + ": " + ioe);
+    }
+
+    Dimension screenSize = getToolkit().getScreenSize();
+    int width = screenSize.width * 8 / 10;
+    int height = screenSize.height * 8 / 10;
+    setBounds(width/8, height/8, width, height);
+    setVisible(true);
+  }
+
+  public void actionPerformed(ActionEvent event) {
+    String url;
+    if (event.getSource() == urlField) 
+      url = urlField.getText();
+    else  // Clicked "home" button instead of entering URL
+      url = initialURL;
+    try {
+      htmlPane.setPage(new URL(url));
+      urlField.setText(url);
+    } catch(IOException ioe) {
+      warnUser("Can't follow link to " + url + ": " + ioe);
+    }
+  }
+
+  public void hyperlinkUpdate(HyperlinkEvent event) {
+    if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+      try {
+        htmlPane.setPage(event.getURL());
+        urlField.setText(event.getURL().toExternalForm());
+      } catch(IOException ioe) {
+        warnUser("Can't follow link to " 
+                 + event.getURL().toExternalForm() + ": " + ioe);
+      }
+    }
+  }
+
+  private void warnUser(String message) {
+    JOptionPane.showMessageDialog(this, message, "Error", 
+                                  JOptionPane.ERROR_MESSAGE);
+  }
+
+  public class JIconButton extends JButton {
+	  public JIconButton(String file) {
+	    super(new ImageIcon(file));
+	    setContentAreaFilled(false);
+	    setBorderPainted(false);
+	    setFocusPainted(false);
+	  }
 	}
 
-	public void hyperlinkUpdate(HyperlinkEvent e) {
-	    if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-	        JOptionPane.showMessageDialog(null,"das");
-	    	try {
-	          htmlPane.setPage(e.getURL());
-	        } catch(IOException ioe) {
-	          // Some warning to user
-	        }
-	      }
-	
-	}
-
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
