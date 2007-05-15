1c920dd9dfc53816d743316b6ad623f1f8c3b447
inigo <inigo>
2007-05-15 14:20:12 +0000

    Depurando codigo redundante

--- SimTraffic/is/SimTraffic/Vista/Acciones/AccionVer.java
+++ SimTraffic/is/SimTraffic/Vista/Acciones/AccionVer.java
@@ -2,11 +2,7 @@ package is.SimTraffic.Vista.Acciones;
 
 import is.SimTraffic.IControlador;
 import is.SimTraffic.IModelo;
-import is.SimTraffic.Herramientas.HBuscarElemento;
 import is.SimTraffic.Mapa.ElementoMapa;
-import is.SimTraffic.Mapa.LineaBus;
-import is.SimTraffic.Mapa.Nodo;
-import is.SimTraffic.Mapa.Tramo;
 import is.SimTraffic.Vista.PanelMapa;
 
 import java.awt.BorderLayout;
@@ -14,7 +10,6 @@ import java.awt.Font;
 import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 import java.util.Iterator;
 
 import javax.swing.BorderFactory;
@@ -25,7 +20,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-public class AccionVer implements ActionListener {
+public abstract class AccionVer implements ActionListener {
 
 	IControlador controlador;
 	IModelo modelo;	
@@ -81,9 +76,7 @@ public class AccionVer implements ActionListener {
 		 }
 	}
 	
-	protected void estableceOyenteBuscar(JButton buscar) {
-		 buscar.addActionListener(new accionBuscar());
-	}
+	abstract protected void estableceOyenteBuscar(JButton buscar) ;
 	
 	protected void poneElementosEnLista(){
 		elementos = new List(); 
@@ -103,30 +96,6 @@ public class AccionVer implements ActionListener {
 			ventanaVer=null;
 			modelo.getMapa().limpiaSeleccion();
 			panel_mapa.repaint();
-		}
-	}
-	
-	private class accionBuscar implements ActionListener {
-
-		public void actionPerformed(ActionEvent arg0) {
-			ArrayList<LineaBus> lista=modelo.getMapa().getLineasAutobuses();
-			if (!lista.isEmpty()){			
-			LineaBus seleccionada = lista.get(elementos.getSelectedIndex());
-			//HBuscarElemento herramientaBuscar = new HBuscarElemento(nombreElemento,seleccionada.getNombre(),panel_mapa);
-			//controlador.herramienta(herramientaBuscar);
-			Iterator tramos = seleccionada.getTramos().iterator();
-			Iterator paradas = seleccionada.getParadas().iterator();
-			
-			//Añadimos cada uno de los tramos  
-			while(tramos.hasNext()){
-			   Tramo aux=(Tramo)tramos.next();
-			   modelo.getMapa().getSeleccion().añadirTramo(aux);  
-			 } 
-			while(paradas.hasNext()){
-			   Nodo parada = (Nodo)paradas.next();
-			   modelo.getMapa().getSeleccion().añadirNodo(parada);
-			}
-			}
 		}
 	}
 
