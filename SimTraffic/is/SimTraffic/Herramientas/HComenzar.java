06f0af080e4c4706081d7f5f56f3fffc1ec4e1a2
pablof <pablof>
2007-05-18 12:34:37 +0000

    Se le pasa ventana

--- SimTraffic/is/SimTraffic/Herramientas/HComenzar.java
+++ SimTraffic/is/SimTraffic/Herramientas/HComenzar.java
@@ -1,19 +1,24 @@
 package is.SimTraffic.Herramientas;
 
+import sun.security.krb5.internal.crypto.m;
 import is.SimTraffic.*;
 import is.SimTraffic.Utils.Tiempo;
+import is.SimTraffic.Vista.Ventana;
+import is.SimTraffic.Vista.EscuchasRaton.MLSeleccionarYMover;
 
 public class HComenzar implements IHerramienta {
 
 	int horas;
 	int minutos;
 	int segundos;
+	Ventana ventana;
 	/**
 	 */
-	public HComenzar(int h, int m, int s) {
+	public HComenzar(Ventana ventana, int h, int m, int s) {
 		horas = h;
 		minutos = m;
 		segundos = s;
+		this.ventana = ventana;
 	}
 
 	/**
@@ -23,6 +28,8 @@ public class HComenzar implements IHerramienta {
 	public int hacer(IModelo modelo) {
 		modelo.getSimulacion().getReloj().traduceATiempo(horas, minutos, segundos);
 		modelo.getSimulacion().comenzar(modelo.getMapa());
+		//ventana.getEscuchaSeleccionar().desactivar();
+		//ventana.setEscuchaSeleccionar(null);
 		return 0;
 	}
 
@@ -32,6 +39,8 @@ public class HComenzar implements IHerramienta {
 	 */
 	public int deshacer(IModelo modelo) {
 		modelo.getSimulacion().detener();
+		//ventana.getEscuchaSeleccionar().activar();
+		//ventana.setEscuchaSeleccionar(new MLSeleccionarYMover(modelo));
 		return 0;
 	}
 	public String toString(){
