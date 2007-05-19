811cb952d51de5f3ab1ccfd0406fbd7f6f486863
namaja <namaja>
2007-05-19 18:24:12 +0000

    creacion de la clase

---
+++ SimTraffic/is/SimTraffic/jUnit/LibreriaIA/Algoritmos/Problema/DistanciaNodos/EstadoDistanciaNodosTest.java
@@ -0,0 +1,46 @@
+package is.SimTraffic.jUnit.LibreriaIA.Algoritmos.Problema.DistanciaNodos;
+
+import is.SimTraffic.LibreriaIA.Problema.DistanciaNodos.EstadoDistanciaNodos;
+import is.SimTraffic.Mapa.Nodo;
+import is.SimTraffic.Mapa.Posicion;
+import junit.framework.TestCase;
+
+/**
+ * Clase de pruebas para la clase EstadoDistanciaNodos
+ *
+ */
+public class EstadoDistanciaNodosTest extends TestCase{
+	
+	/**
+	 * Atributo que es un nodo necesario para las pruebas
+	 */
+	Nodo posicion;
+	/**
+	 * 
+	 */
+	EstadoDistanciaNodos estado;
+	
+	/**
+	 * 
+	 * @param arg0
+	 */
+	public EstadoDistanciaNodosTest(String arg0) {
+		super(arg0);
+	}
+	
+	protected void setUp() throws Exception {
+		posicion = new Nodo(new Posicion(100,100));
+		estado = new EstadoDistanciaNodos(posicion);
+	}
+	
+	public void testequals()
+	{
+		Nodo aux = new Nodo(new Posicion (10,50));
+		EstadoDistanciaNodos estado= new EstadoDistanciaNodos(aux); 
+		assertTrue(!(estado.equals(this.estado)));
+		assertTrue((estado.equals(estado)));
+	}
+	
+	
+	
+}
